7e4b8be8705734e7842201914a86ff1b848f236a
Emil B <emkaran_500@mail.ru>
2024-01-11 13:07:23 +0400

    Fix EB_DZ_63

--- EB_DZ_63/EB_DZ_63.js
+++ EB_DZ_63/EB_DZ_63.js
@@ -20,14 +20,26 @@ var expensiveProducts = products.filter((product) => product.price >= 20);
 console.log('expensive products' ,expensiveProducts);
 
 //Задачка номер 3
+const products2 = [
+  { id: 1, name: "Футболка", price: 20 },
+  { id: 2, name: "Шорты", price: 25 },
+  { id: 3, name: "Носки", price: 8 },
+  { id: 4, name: "Платье", price: 50 }
+];
 const purchasedProductsIds = [1, 3, 4];
 function sumOfPurchased(products, purchasedProductsIds)
 {
     var purchasedSum = 0;
-    for (let id of purchasedProductsIds)
+    for (let product of products)
     {
-        purchasedSum += products[id-1]['price'];
+        for (let id of purchasedProductsIds)
+        {
+            if (product.id == id)
+            {
+                purchasedSum += product.price;
+            }
+        }
     }
     console.log('purchasedSum', purchasedSum);
 }
-sumOfPurchased(products, purchasedProductsIds);
+sumOfPurchased(products2, purchasedProductsIds);
