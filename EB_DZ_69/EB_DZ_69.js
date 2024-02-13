289745ef75c15872b4de96e9f400edf66cec5d12
Emil B <emkaran_500@mail.ru>
2024-02-13 19:21:02 +0400

    Finish EB_DZ_69

--- EB_DZ_69/EB_DZ_69.js
+++ EB_DZ_69/EB_DZ_69.js
@@ -1,12 +1,33 @@
 const body = document.body;
 
-function addInfo(objData, parentSquare)
+function addInfo(objData, parentSquare, inDepth = false)
 {
     if (objData.__proto__ == Array.prototype)
     {
         objData.forEach(objCredential => {
             const userSquare = document.createElement('ul');
             userSquare.classList.add('ul-square');
+
+            if (!inDepth)
+            {
+                userSquare.addEventListener('click', (e) =>
+                {
+                    if (userSquare.parentElement.parentElement.tagName.toLowerCase() == 'body' && !userSquare.classList.contains('ul-with-posts'))
+                    {
+                        fetch(`https://jsonplaceholder.typicode.com/posts/?userId=${objCredential['id']}`)
+                        .then(response => response.json())
+                        .then(postsArr => 
+                            {
+                                const posts = document.createElement('li');
+                                posts.textContent = 'user posts';
+                                addInfo(postsArr, posts, true);
+                                userSquare.appendChild(posts);
+                                userSquare.classList.add('ul-with-posts');
+                            });
+                    }
+                })
+            }
+            
             const credentials = Object.keys(objCredential);
             credentials.forEach(credential => {
                 if (typeof(objCredential[credential]) == 'object')
@@ -35,22 +56,21 @@ function addInfo(objData, parentSquare)
     {
         const subFields = Object.keys(objData);
         const subCredentials = document.createElement('ul');
-        subCredentials.classList.add('ul-square');
         subFields.forEach(subField => {
             if (objData[subField].__proto__ != Object.prototype)
             {
                 const subData = document.createElement('li');
                 subData.textContent = subField + ": " + objData[subField];
-                subCredentials.appendChild(subData);
+                parentSquare.appendChild(subData);
             }
             else
             {
                 const subData = document.createElement('li');
-                subData.textContent = subField;
                 const currentSquare = document.createElement('ul');
-                currentSquare.classList.add('ul-square');
-                addInfo(objData[subField], subCredentials);
+                subData.textContent = subField;
+                addInfo(objData[subField], currentSquare);
                 subData.appendChild(currentSquare);
+                parentSquare.appendChild(subData);
             }
         });
         parentSquare.appendChild(subCredentials);
